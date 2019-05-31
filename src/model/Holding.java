f943cff8931b254eeae3a061ee3b67ff92f41471
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-05-31 11:51:21 -0500

    se agrega recorrido espiral

--- src/model/Holding.java
+++ src/model/Holding.java
@@ -11,5 +11,63 @@ public class Holding extends LegalPersonality{
 		edifice = new Cubicle [FILAS][COLUMNAS];
 	}
 	
+	public String spiral(String position){
+		String msj=" ";
+		int avanceColumnas = 0;
+		int sumador= 0;
+		int restador=1;
+		boolean a= false;
+		boolean b= false;
+		boolean c= false;
+		
+		while(edifice = edifice[0].length*edifice[0].length){
+			
+			// movimiento columnas abajo 
+			for(int i = 0; i<edifice[avanceColumnas].length-sumador; i++){
+				// edifice[i][avanceColumnas]
+				if(edifice[i][avanceColumnas].getPosition().equals(position)){
+					msj+=edifice[i][avanceColumnas].getEmail();
+				}
+				int q = edifice[avanceColumnas].length-sumador;
+				a = true;
+			}
+			// movimiento fila adelante
+			if(a == true){
+				for(int j = q; j<edifice[q].length-sumador; j++){
+					if(edifice[q][j].getPosition().equals(position)){
+						msj+=edifice[q][j].getEmail();
+					}
+					int r = edifice[q].length-sumador;
+					b = true;
+				}
+			}
+			// movimiento colummnas arriba
+			if(b==true){
+				for(int f = r; f>sumador; f--){
+					if(edifice[f][r].getPosition().equals(position)){
+						msj+=edifice[r][f].getEmail();
+					}
+					int s = sumador;
+					c = true;
+				}
+			}
+			// movimiento fila atras
+			if(c== true){
+				for(int k = edifice[sumador].length-1; k<restador; k--){
+					if(edifice[s][k].getPosition().equals(position)){
+						msj+=edifice[s][k].getEmail();
+					}
+				}
+			}
+			
+			a= false;
+			b= false;
+			c= false;
+			sumador++;
+			restador++;
+			avanceColumnas++;
+		}
+		return msj;
+	}
 	
 }
