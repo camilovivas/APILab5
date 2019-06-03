7b841b9f6306bc0700f21a810093d4906e0f8a47
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-06-02 22:13:46 -0500

    se corrigen errores y se agregan metodos de busqueda

--- src/model/Holding.java
+++ src/model/Holding.java
@@ -19,16 +19,20 @@ public class Holding extends LegalPersonality{
 		boolean a= false;
 		boolean b= false;
 		boolean c= false;
+		int q = edifice.length-sumador;
+		int r = edifice[q].length-sumador;
+		int s = sumador;
 		
-		while(edifice = edifice[0].length*edifice[0].length){
+		
+		while(edifice == FILAS*COLUMNAS){
 			
 			// movimiento columnas abajo 
-			for(int i = 0; i<edifice[avanceColumnas].length-sumador; i++){
+			for(int i = 0; i<edifice.length-sumador; i++){
 				// edifice[i][avanceColumnas]
 				if(edifice[i][avanceColumnas].getPosition().equals(position)){
 					msj+=edifice[i][avanceColumnas].getEmail();
 				}
-				int q = edifice[avanceColumnas].length-sumador;
+				
 				a = true;
 			}
 			// movimiento fila adelante
@@ -37,7 +41,7 @@ public class Holding extends LegalPersonality{
 					if(edifice[q][j].getPosition().equals(position)){
 						msj+=edifice[q][j].getEmail();
 					}
-					int r = edifice[q].length-sumador;
+					
 					b = true;
 				}
 			}
@@ -47,7 +51,6 @@ public class Holding extends LegalPersonality{
 					if(edifice[f][r].getPosition().equals(position)){
 						msj+=edifice[r][f].getEmail();
 					}
-					int s = sumador;
 					c = true;
 				}
 			}
@@ -70,4 +73,118 @@ public class Holding extends LegalPersonality{
 		return msj;
 	}
 	
+	public String SearchExtensionL(String name){
+		String ext;
+		boolean found = false;
+		int a = edifice[0].length;
+		for(int i = 0; i<edifice.length && !found; i++){
+			if(edifice[i][0].getNameEmployee().equals(name)){
+				ext = edifice[i][0].getExtension();
+				found = true;
+			}
+			
+		}
+		for(int j = a; j<edifice[a].length && !found; j++){//como hacer que pase de un for a otro
+			if(edifice[a][j].getNameEmployee().equals(name)){
+				ext = edifice[a][j].getExtension();
+				found = true;
+			}
+			
+		}
+		return ext;
+	}
+	
+	public String SearchExtensioZ(String name){
+		String ext;
+		boolean found = false;
+		int  r = edifice.length;
+		int b = edifice[0].length;
+		
+		for(int i = 0; i<edifice[0].length && !found; i++){
+			if(edifice[0][i].getNameEmployee().equals(name)){
+				ext = edifice[0][i].getExtension();
+				found = true;
+			}
+			
+		}
+		
+		// recorre diagonal
+		for(int j = 0; j<r++ && !found; j++){
+			for(int k = b; k>0 ;k--){
+					
+				if(edifice[j][k].getNameEmployee().equals(name)){
+					ext = edifice[j][k].getExtension();
+					found = true;
+				}
+			}
+		}
+		
+		for(int f = 0; f<edifice[r].length; f++){
+			if(edifice[r][f].getNameEmployee().equals(name)){
+				ext = edifice[r][f].getExtension();
+				found = true;
+			}
+			
+		}
+		return ext;
+	}
+	
+	public String SearchExtensionX(String name){
+		String ext;
+		int b = edifice[0].length;
+		int c =  edifice.length;
+		// diagonal de paa atras
+		for(int i = 0; i<c++; i++){
+			for(int j = b; j<0; j--){
+				if(edifice[i][j].getNameEmployee().equals(name)){
+					ext = edifice[i][j].getExtension();
+				}
+			}
+		}
+		// diagonal hacia adelante
+		for(int k = 0; k<c++; k++){
+			for(int h = 0; h<b; h++){
+				if(edifice[k][h].getNameEmployee().equals(name)){
+					ext = edifice[k][h].getExtension();
+				}
+			}
+		}
+		return ext;
+	}
+	// con el encontrado diferente ya no entra a los demas ciclos?
+	public String SearchExtensionO(String name){
+		String ext;
+		int e =edifice[0].length;
+		int d = edifice.length;
+		for(int i = 0; i<e; i++){
+			if(edifice[0][i].getNameEmployee().equals(name)){
+				ext = edifice[0][i].getExtension();
+			}
+		}
+		for(int r = 0; r<d; r++){
+			if(edifice[r][e].getNameEmployee().equals(name)){
+				ext =edifice[r][e].getExtension();		
+			}
+		}
+		for(int y = e; y < 0; y--){
+			if(edifice[d][y].getNameEmployee().equals(name)){
+				ext = edifice[d][y].getExtension();		
+			}
+		}
+		for(int j = d; j< 0 ; j--){
+			if(edifice[j][0].getNameEmployee().equals(name)){
+				ext = edifice[j][0].getExtension();	
+			}
+		}
+		return ext;
+	}	
+	
+	public String SearchExtensionE(String name){
+		if(FILAS == COlUMNAS){
+			
+			
+		}
+		
+		
+	}	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,67 @@
+/**
+*Universidad Icesi (Cali-Colombia)
+*laboratorio 5 y 6 APO I
+*@autor: Camilo Vivas <camilo-152000@hotmail.com>
+*Date: 09/06/2019
+*/
 package ui;
 import model.*;
 import java.util.Scanner;
 
-public static void main(String [] args){
-	Scanner reader = new Scanner (System.in);
+public class Main{
 	private Holding relacion;
+	private Scanner reader;
+	
+	public Main(){
+		init();
+		reader = new Scanner (System.in);
+		
+	}
+	public static void main(String [] args){
+		Main m = new Main();
+		
+	}
+	public void ShowMenu(){
+		int option = 0;
+		while(option =! 4){
+			System.out.println("ingrese el numeor de la opcion que desea realizar");
+			System.out.println("1. 
+			System.out.println("2. 
+			System.out.println("3. buscar extencion de un trabajador");
+			System.out.println("4. ver los correos electronicos de los trabajadores de un cargo");
+			
+			
+			
+			option = reader,nextInt();
+			switch(option){
+			case :
+		
+		
+			case :
+		
+		
+			case :
+		
+		
+			case :
+			System.out.println("ingrse el nombre del trabajador");
+			String name = reader.next();
+			System.out.println("ingrese numero del metodo por el cual desea realizar la busqueda");
+			System.out.println("1.busqueda en L");
+			System.out.println("2.busqueda en Z");
+			System.out.println("3.busqueda en X");
+			System.out.println("4.busqueda en O");
+			System.out.println("5.busqueda en E");
+			int method = reader.nextInt();
+		
+			case :
+
+		}
+	
+	}
+	public void init()[
+	
+	
+	}
+
 }
